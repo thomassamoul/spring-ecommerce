a6a4a8a833f7850f3fc6a219412e78b11de69057
Thomas Samoul <thomas01997z20@gmail.com>
2023-06-16 15:57:15 +0300

    Added login feature added jwt and password encryption

---
+++ src/main/java/com/example/ecommerce/api/model/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.api.model;
+
+public class LoginResponse {
+    private String jwt;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
--- src/main/java/com/example/ecommerce/service/UserService.java
+++ src/main/java/com/example/ecommerce/service/UserService.java
@@ -1,18 +1,25 @@
 package com.example.ecommerce.service;
 
+import com.example.ecommerce.api.model.LoginBody;
 import com.example.ecommerce.api.model.RegistrationBody;
 import com.example.ecommerce.exception.UserAlreadyExistsException;
 import com.example.ecommerce.model.LocalUser;
 import com.example.ecommerce.model.dao.LocalUserDAO;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     private LocalUserDAO localUserDAO;
+    private EncryptionService encryptionService;
+    private JWTService jwtService;
 
-    public UserService(LocalUserDAO localUserDAO) {
+    public UserService(LocalUserDAO localUserDAO, EncryptionService encryptionService, JWTService jwtService) {
         this.localUserDAO = localUserDAO;
+        this.encryptionService = encryptionService;
+        this.jwtService = jwtService;
     }
 
     public LocalUser register(RegistrationBody registrationBody) throws UserAlreadyExistsException {
@@ -26,7 +33,19 @@ public class UserService {
         user.setFirstName(registrationBody.getFirstName());
         user.setLastName(registrationBody.getLastName());
 
-        user.setPassword(registrationBody.getPassword());
+        user.setPassword(encryptionService.encryptPassword(registrationBody.getPassword()));
         return localUserDAO.save(user);
+    }
+
+    public String loginUser(LoginBody loginBody) {
+        Optional<LocalUser> opUser = localUserDAO.findByUsernameIgnoreCase(loginBody.getUsername());
+        if (opUser.isPresent()) {
+            LocalUser user = opUser.get();
+            if (encryptionService.verifyPassword(loginBody.getPassword(), user.getPassword())) {
+                return jwtService.generateJWT(user);
+
+            }
+        }
+        return null;
     }
 }
